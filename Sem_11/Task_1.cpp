aa18d2e162e225068400b54f71a21bca8f17a9e0
aleksis.datseris <aleksis.datseris@ontotext.com>
2024-12-16 23:50:02 +0200

    add week 11 solutions

---
+++ Sem_11/Task_1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+std::vector<int> twoSum(std::vector<int>& nums, int target)
+{
+    std::unordered_map<int, int> map;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int completement = target - nums[i];
+
+        if (map.find(completement) == map.end())
+            map[nums[i]] = i;
+        else
+            return { map[completement], i };
+    }
+
+    return {};
+}
+
+int main()
+{
+
+}
