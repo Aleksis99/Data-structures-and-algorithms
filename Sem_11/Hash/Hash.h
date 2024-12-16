58632555541e0de2f9d6fbaf0a1bbf305930eb8f
aleksis.datseris <aleksis.datseris@ontotext.com>
2024-12-16 17:09:57 +0200

    add week 11

---
+++ Sem_11/Hash/Hash.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+class HashingFunction
+{
+public:
+	virtual ~HashingFunction() = default;
+
+	// Calculates the hash of an integer
+	virtual size_t calculateHashCode(int key) = 0;
+};
+
+class ModAndMultHashingFunction : public HashingFunction
+{
+private:
+	size_t mod;
+	size_t multiplier;
+
+public:
+	ModAndMultHashingFunction(size_t mod, size_t multiplier) : mod(mod), multiplier(multiplier)
+	{
+	}
+
+	size_t calculateHashCode(int key) override
+	{
+		return (key % mod) * multiplier;
+	}
+};
+
+template <typename ValueType>
+class Hash
+{
+protected:
+	HashingFunction *hashFunc;
+
+public:
+	Hash(HashingFunction *hashFunc = nullptr) : hashFunc(hashFunc)
+	{
+	}
+	virtual ~Hash() = default;
+
+	virtual void insert(int key, const ValueType &value) = 0;
+	bool find(int key, const ValueType &output) const = 0;
+	virtual bool remove(int key) = 0;
+};
